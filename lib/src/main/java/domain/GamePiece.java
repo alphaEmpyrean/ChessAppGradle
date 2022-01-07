41b23037ce45e0323884d01540d30d5efff5ee55
alphaEmpyrean <dev@alphaempyrean.com>
2022-01-07 16:41:42 -0600

    Adds comments, removes constructor & toString from GamePiece.java

--- lib/src/main/java/domain/GamePiece.java
+++ lib/src/main/java/domain/GamePiece.java
@@ -1,19 +1,19 @@
 package domain;
-
+/**
+ * Abstract class that represents the concept of a game piece.
+ * 
+ * @author JoshuaKnippel
+ *
+ */
 public abstract class GamePiece {
 	
 	protected String textRepresentation;
-
-	public GamePiece() {
-		// TODO Auto-generated constructor stub
-	}
 	
+	/**
+	 * Returns reference to the instances testRepresentation member.
+	 * @return
+	 */
 	public String getTextRepresentation() { 
 		return this.textRepresentation;
 	}
-	
-	public String toString() {
-		return textRepresentation;
-	}
-
 }
