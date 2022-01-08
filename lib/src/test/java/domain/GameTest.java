1b9127b56c2dee854ac61c75c5f123c0c3bae88e
alphaEmpyrean <dev@alphaEmpyrean.com>
2022-01-08 03:14:49 -0600

    Add java doc comments to GameTest.java

--- lib/src/test/java/domain/GameTest.java
+++ lib/src/test/java/domain/GameTest.java
@@ -10,16 +10,32 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
 
+/**
+ * This is a junit test for the Game.java class.
+ * 
+ * @author JoshuaKnippel
+ *
+ */
 @DisplayName("Test Game Class")
 public class GameTest {
 	
 	static Game game;
 	
+	/**
+	 * Builds new game instance before each test
+	 */
 	@BeforeEach
 	public void beforeEach () {
 		game = new Game();
 	}
 	
+	/**
+	 * Tests to make sure that all 64 indices of the gameBoad 
+	 * array[8][8] contain a reference to a BoardSpace object. 
+	 * Imports a .csv of gameBoard coordinates as an argument source.
+	 * @param r
+	 * @param c
+	 */
 	@ParameterizedTest(name = "{index}> c: {0},{1}")
 	@CsvFileSource(resources = "/boardCoordinates.csv")
 	@DisplayName("All indices of 8 x 8 gameboard array contain a BoardSpace object reference")
@@ -27,6 +43,9 @@ public class GameTest {
 		assertEquals(game.getBoardSpace(r, c).getClass(), BoardSpace.class, "Not a BoardSpace.class object");		
 	}
 	
+	/**
+	 * Test to ensure get() returns correct gameBoard after a set()
+	 */
 	@Test
 	@DisplayName ("gameBoard getter/setter returns correct value")
 	public void testGetSetGameBoard( ) {
@@ -35,6 +54,9 @@ public class GameTest {
 		assertEquals(gameBoard, game.getGameBoard());		
 	}
 	
+	/**
+	 * Tests to ensure get() returns the correct BoardSpace.
+	 */
 	@Test
 	@DisplayName("BoardSpace getter returns correct value")
 	public void testGetBoardSpace() {
@@ -44,6 +66,17 @@ public class GameTest {
 		assertEquals(gameBoard[0][0], game.getBoardSpace(0, 0));
 	}
 	
+	/**
+	 * Tests to ensure that the game board is populated with 
+	 * the game pieces and that they are in the correct locations.
+	 * 
+	 * Uses a .csv file source for argument list. The .csv contains 
+	 * a coordinate -> piece 'mapping'. Uses the {@link StringToClassConverter.class} 
+	 * to prevent the need to add the package name in the csv.
+	 * @param r
+	 * @param c
+	 * @param gp
+	 */
 	@ParameterizedTest(name = "{index}>	c: {0},{1} contains {2}")
 	@CsvFileSource(resources = "/pieceStartingCoordinates.csv")
 	@DisplayName("Game pieces placed in correct starting positiion")
